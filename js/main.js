7ff7f79a2fddd5c3cea9b6f1c12a51a7a4f49957
Guilherme Vanini <guilhermeev3@gmail.com>
2021-09-28 11:10:47 -0300

    introducao ao javascript

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var nome = "Guilherme Vanini";
+var idade = 21;
+var idade2 = 10;
+var n1 = 4;
+var n2 = 2;
+var frase = "Japão é um país tecnológico"
+
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toLowerCase());
+console.log(n1 * n2);
+//console.log(frase.toUpperCase());
+//console.log(frase.replace("Japão", "China"));
+
+//alert(frase.replace("Japão", "China"));
